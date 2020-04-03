339d2c1846fcbc1453b9d887a5aad552af21bcff
Lucas <lama2@cin.ufpe.br>
2020-04-03 19:14:35 -0300

    fix pip2 install error

--- installSuricata.sh
+++ installSuricata.sh
@@ -1,6 +1,9 @@
-VER=4.1.6
+VER=5.0.2
 
-sudo apt -y install python-pip python3-pip tshark wireshark git libpcre3 libpcre3-dbg libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 make libmagic-dev libjansson-dev libjansson4 pkg-config libnspr4-dev libnss3-dev liblz4-dev rustc cargo libhtp-dev
+sudo apt-add-repository universe
+sudo apt update
+sudo apt -y install software-properties-common 
+sudo apt -y install python3-pip tshark wireshark git libpcre3 libpcre3-dbg libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 make libmagic-dev libjansson-dev libjansson4 pkg-config libnspr4-dev libnss3-dev liblz4-dev rustc cargo libhtp-dev
 
 pip3 install pyshark
 
