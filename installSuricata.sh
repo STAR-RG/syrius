c15a1d9b6eea3e34a073e9b4d8c7b56500c8d4b5
Lucas <lama2@cin.ufpe.br>
2020-04-05 12:38:32 -0300

    updated for ubuntu 20.04 - updated suricata version to 4.1.7

--- installSuricata.sh
+++ installSuricata.sh
@@ -1,9 +1,9 @@
-VER=5.0.2
+VER=4.1.7
 
 sudo apt-add-repository universe
 sudo apt update
 sudo apt -y install software-properties-common 
-sudo apt -y install python3-pip tshark wireshark git libpcre3 libpcre3-dbg libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 make libmagic-dev libjansson-dev libjansson4 pkg-config libnspr4-dev libnss3-dev liblz4-dev rustc cargo libhtp-dev
+sudo apt -y install python2 python3-pip tshark wireshark git libpcre3 libpcre3-dbg libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 make libmagic-dev libjansson-dev libjansson4 pkg-config libnspr4-dev libnss3-dev liblz4-dev rustc cargo libhtp-dev
 
 pip3 install pyshark
 
@@ -24,8 +24,8 @@ sudo make install
 cd ../../suricata-update
 git clone https://github.com/OISF/suricata-update.git
 cd suricata-update
-python setup.py build
-sudo python setup.py install
+python2 setup.py build
+sudo python2 setup.py install
 
 cd ../..
 
